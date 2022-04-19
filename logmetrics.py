f26066fe4707edc12571789d7ee14507962872ac
Simon-Pierre Allaire <spa05@acuitysso.com>
2022-04-18 23:32:52 -0400

    Add quotes around metrics labels value

--- logmetrics.py
+++ logmetrics.py
@@ -5,11 +5,20 @@ class LogMetrics:
         self.output = ""
         self.endl = " \n"
 
-    def add_docker_partitition_usage(self):
-        cmd_output = subprocess.check_output(["df", "-h", "/var/lib/docker"]).decode("utf-8")
-        self.output += "docker_partition_usage_percent{id=0} " + cmd_output.split("\n")[1].split()[4][:-1] + self.endl
-        self.output += "docker_partition_available_gb{id=0} " + cmd_output.split("\n")[1].split()[3][:-1] + self.endl
-
+    def add_docker_partitition_usage(self, id=0):
+        cmd_output = subprocess.check_output(["df", "-h", "/var/lib/docker"]).decode(
+            "utf-8"
+        )
+        self.output += (
+            f"docker_partition_usage_percent{{id=\"{id}\"}} "
+            + cmd_output.split("\n")[1].split()[4][:-1]
+            + self.endl
+        )
+        self.output += (
+            f"docker_partition_available_gb{{id=\"{id}\"}} "
+            + cmd_output.split("\n")[1].split()[3][:-1]
+            + self.endl
+        )
 
     def __str__(self):
         return self.output
